eb48ac4bb493da33a0119a2ec2521d7a36adfa3c
PrashantShri <meprashant1994@gmail.com>
2024-10-08 20:53:27 +0530

    Update hi.java

--- hi.java
+++ hi.java
@@ -2,6 +2,6 @@ public class hi{
     public static void main(String[] args) {
         // Print Hello World to the console
         System.out.println("Hello, World! This is prashant shrivastava, How can I help you");
-        System.out.println("Hello Ashwini , I update the code plese try now.");
+        System.out.println("Hello Ashwini");
     }
 }
